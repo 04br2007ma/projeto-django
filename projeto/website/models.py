8e8c8e8e428f0d2a7c74ceeb7f93bdc42714322b
04br2007ma <0000111663854xsp@al.educacao.sp.gov.br>
2025-09-10 20:52:35 -0300

    new

--- projeto/website/models.py
+++ projeto/website/models.py
@@ -7,5 +7,11 @@ class Pessoa(models.Model):
     email = models.EmailField(unique=True)
     data_nascimento = models.DateField(null=True, blank=True)
 
+
+class dieta(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+
     def __str__(self):
         return self.nome
